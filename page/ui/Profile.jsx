b55b1666517afc9a6f3fb43e3fdb299e42898d34
yesu4658 <yesubabu.gudi@gmail.com>
2021-07-24 15:43:06 +0530

    changed background color and added location in profile

--- page/ui/Profile.jsx
+++ page/ui/Profile.jsx
@@ -1,3 +1,6 @@
+import LocationCityIcon from "@material-ui/icons/LocationCity";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
 const Profile = () => {
     return (
         <div>
@@ -14,7 +17,19 @@ const Profile = () => {
                         <h2 id="name">Yesu Babu .G</h2>
                     </div>
                     <h4>age:22</h4>
+
                     <h5>(Full-stack Developer)</h5>
+                    <p>
+                        <LocationOnIcon
+                            style={{
+                                fontSize: 15,
+                                marginTop: "0.3em",
+                                marginRight: "0.5em",
+                            }}
+                        />
+                        vijayawada,
+                        <small style={{ fontWeight: "normal" }}>(India)</small>
+                    </p>
                     <p>|--------- Bio ---------|</p>
                     <p>
                         <i>
@@ -22,6 +37,7 @@ const Profile = () => {
                             time ```
                         </i>
                     </p>
+
                     <ul>
                         <li>loves cricket</li>
                         <li>watching anime &#128512;</li>
